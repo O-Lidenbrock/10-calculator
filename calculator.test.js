b0ed0d2e4d3a0e6b84857d09a41112cf4ebfea05
butler <113191739+SchrodingersMouser@users.noreply.github.com>
2025-05-22 14:42:49 -0500

    clearing the node files

--- calculator.test.js
+++
@@ -1,9 +0,0 @@
-const calculator = require('./calculator');
-
-test('adds integers', () => {
-  expect(calculator.add(8,2)).toBe(10);
-})
-
-// test('adds floats', () => {
-//   expect(calculator.add(8,2)).toBe(10);
-// })
